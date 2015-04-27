6e9b33777b5082a76d2ff997ba9a1a2ff1e44a0a
ansari007 <mohamadansari1994@gmail.com>
2015-04-27 11:47:09 +0300

    resolving push controller

---
+++ SayedAlShohada/Controllers/PushController.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Repository;
+using Infrastructure.Repository.Imp;
+using SayedAlShohada.Models;
+using SayedAlShohada.Models.Mappers;
+using SayedAlShohada.Models.Mappers.Imp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SayedAlShohada.Controllers
+{
+    public class PushController : ApiController
+    {
+        private IPushRepository _pushRepository;
+        private IPushMapper _pushMapper;
+        public PushController()
+        {
+            _pushRepository = new PushRepository();
+            _pushMapper = new PushMapper();
+        }
+
+        [HttpPost]
+        public void InsertDevice([FromBody] PushForm form)
+        {
+
+            var map = _pushMapper.Map(form);
+            _pushRepository.InsertDevice(map);
+
+        }
+
+    }
+}
